edab1c8f88a8fd3e22b440c2f5642c21c1b7574c
nikolanicic <nikolanicic1995@gmail.com>
2024-08-05 23:28:44 +0200

    python commands

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from src.app import app
+# def before_request():
+#   if not request.is_secure:
+#     url = request.url.replace('http://', 'https://', 1)
+#     code = 301
+#     return redirect(url, code=code)
+
+# export LC_ALL=C
+# sudo python3 -m venv venv
+# . venv/bin/activate
 
 if __name__ == '__main__':
   app.run(debug=True)
